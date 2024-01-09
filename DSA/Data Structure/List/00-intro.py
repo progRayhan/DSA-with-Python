0da2feff0bc429715f26c1b661f16a83e2033679
Rayhan <prog.rayhan1@gmail.com>
2024-01-09 11:21:38 -0800

    list intro
    1.intro of list
    
    2.basic uses of list

---
+++ DSA/Data Structure/List/00-intro.py
@@ -0,0 +1,10 @@
+list_one = [1, 2, "Hello", 5.84]
+
+second_element_of_list = list_one[1]
+print(second_element_of_list)
+
+list_one[2] = 3.45 # mutable
+print(list_one)
+
+list_one.append(300)
+print(list_one)
---
+++ DSA/Data Structure/List/01-basic_uses.py
@@ -0,0 +1,6 @@
+student_id_number = [17648580, 18453284, 17483958, 16587428]
+
+# get individual student id number
+for id_number in student_id_number:
+    print(f"id number is: {id_number}")
+    
