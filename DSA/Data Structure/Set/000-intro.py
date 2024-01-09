491c7d0f7d4fd3fa33ed7c3c0d9e915b5cda0336
Rayhan <prog.rayhan1@gmail.com>
2024-01-09 12:00:27 -0800

    feat: set intro

---
+++ DSA/Data Structure/Set/000-intro.py
@@ -0,0 +1,16 @@
+set_one = {1,2,3,4} # only contains unique value
+print(set_one)
+
+set_two = {1,1,2,2,3,4} # duplicate value are ignored
+print(set_two)
+
+is_set_one_and_two_are_equal = (set_one == set_two)
+print(is_set_one_and_two_are_equal)
+
+set_one.add(50) # mutable
+print(set_one)
+
+demo_set = set()
+demo_set.add(5)
+demo_set.add(10)
+print(demo_set)
